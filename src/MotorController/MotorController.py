e75d16db88b938c372dbb4e93861769a3808b604
Adam Feldscher <afeldscher@gmail.com>
2017-09-30 19:09:14 +0000

    adding Motor Controller

---
+++ src/MotorController/MotorController.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+#Motor Controller for Raspberry PI with 16-channel PWM Servo Hat
+#For DART 8/26/17
+#By Adam Feldscher
+#Takes input from -1 to 1 and sets the servo pwm value
+#May need to add mid lower and upper configurable parameters
+
+
+from ABE_ServoPi import PWM
+import time
+from ABE_helpers import ABEHelpers
+import sys
+import RPi.GPIO as GPIO
+
+class MotorController:
+	def __init__(self, pwmPort, dirPort):
+		# Initialise the PWM device using the default address
+		i2c_helper = ABEHelpers()
+		bus = i2c_helper.get_smbus()
+		
+		self.pwm = PWM(bus, 0x40)
+		self.pwm.set_pwm_freq(50) # Set frequency to 60 Hz
+		self.pwm.output_enable()
+		self.pwmPort = pwmPort
+		self.dirPort = dirPort
+	
+		self.upper = 4095  #0 -> 4095
+
+		#init GPIO
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(dirPort, GPIO.OUT)
+		
+
+	#set a value from -1 to 1
+	def setValue(self, value):
+		inVal = float(value)
+		outVal = 0
+		if (inVal > 1 or inVal < -1):
+			raise Exception("Value out of bounds, [-1, 1]")
+	
+		outVal = abs(inVal) * self.upper
+
+		#Set Direction
+		GPIO.output(self.dirPort, (inVal < 0))
+
+
+		#print outVal #<-- for debugging uncomment	
+		self.pwm.set_pwm(self.pwmPort, 0, int(outVal))
+
+
+	#set the motor to off
+	def off(self):
+		self.pwm.set_pwm(self.pwmPort, 0, 0)
+
+
+#===Test driver to control a servo===
+def testDriver():
+	pwmPort = int(input("Enter a PWM port: "))
+	dirPort = int(input("Enter a dir port: "))
+	ctrl = MotorController(pwmPort, dirPort)
+
+	while (True):
+		value = input("Enter a value from -1 to 1: ")
+		if str(value) == "off":
+			ctrl.off()
+		else:	
+			ctrl.setValue(float(value))
+
+
+if __name__ == "__main__":
+	testDriver()
+
+
