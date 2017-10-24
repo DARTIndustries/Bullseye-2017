c25eb346ac50d764d6a4acc82369a906f848bc37
Adam Feldscher <aff39@drexel.edu>
2017-10-23 22:24:37 -0400

    look at threading

--- src/DART/Bullseye/main.py
+++ src/DART/Bullseye/main.py
@@ -7,7 +7,7 @@
 import sys
 sys.path.insert(0, '../../')
 from DART.Bullseye.Utils.AsciiArt import AsciiArt
-
+import threading
 
 
 asciiArt = AsciiArt()
@@ -19,6 +19,9 @@ def run() :
     print(asciiArt.getBullseye(), asciiArt.getBy(), asciiArt.getDart())
     print("\n\t    -----v" + version + "-----\n")
 
+#https://docs.python.org/3/library/threading.html
+#    mythread = MyThread(name = "Thread-{}".format(x + 1))  # ...Instantiate a thread and pass a unique ID to it
+#    mythread.start()             
 
 
 if __name__ == "__main__":
