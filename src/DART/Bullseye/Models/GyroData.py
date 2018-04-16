dafe9f46cc51e00b75827d331b18b2fab60c03a8
Adam Feldscher <aff39@drexel.edu>
2018-04-16 16:34:28 -0400

    adding types

--- src/DART/Bullseye/Models/GyroData.py
+++ src/DART/Bullseye/Models/GyroData.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 
 import sys
+from typing import NamedTuple
+
 sys.path.insert(0, '../../../')
 
-#<timeMS>, <accelX>, <accelY>, <accelZ>, <gyroX>, <gyroY>, <gyroZ>, <magX>, <magY>, <magZ>, <angleX>, <angleY>, <angleZ>
+
+class Coordinate(NamedTuple):
+    x: float
+    y: float
+    z: float
+
+
+# <timeMS>, <accelX>, <accelY>, <accelZ>, <gyroX>, <gyroY>, <gyroZ>, <magX>, <magY>, <magZ>, <angleX>, <angleY>, <angleZ>
 
 class GyroData:
     def __init__(self):
         self.time = 0
-        self.accel = [0.0, 0.0, 0.0]
-        self.gyro = [0.0, 0.0, 0.0]
-        self.mag = [0.0, 0.0, 0.0]
-        self.angle = [0.0, 0.0, 0.0]
+        self.accel: Coordinate = Coordinate(0.0, 0.0, 0.0)
+        self.gyro: Coordinate = Coordinate(0.0, 0.0, 0.0)
+        self.mag: Coordinate = Coordinate(0.0, 0.0, 0.0)
+        self.angle: Coordinate = Coordinate(0.0, 0.0, 0.0)
 
-    def __str__(self):
-        return "GyroData:\n  accel: {0}\n  gyro: {1}\n  mag: {2}\n  angle: {3})".format(self.accel, self.gyro, self.mag, self.angle)
+    def __str__(self) -> str:
+        return "GyroData:\n  accel: {0}\n  gyro: {1}\n  mag: {2}\n  angle: {3})".format(self.accel, self.gyro, self.mag,
+                                                                                        self.angle)
+
+
