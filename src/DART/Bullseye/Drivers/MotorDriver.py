d951e37cc3237038a0da5533965fe8a10fb817ea
Adam Feldscher <aff39@drexel.edu>
2018-04-20 12:55:07 -0400

    FOrmat

--- src/DART/Bullseye/Drivers/MotorDriver.py
+++ src/DART/Bullseye/Drivers/MotorDriver.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-#Motor Driver for Raspberry PI with 16-channel PWM Servo Hat
-#For DART 8/26/17
-#By Adam Feldscher
-#Takes input from -1 to 1 and sets the servo pwm value
-#May need to add mid lower and upper configurable parameters
+# Motor Driver for Raspberry PI with 16-channel PWM Servo Hat
+# For DART 8/26/17
+# By Adam Feldscher
+# Takes input from -1 to 1 and sets the servo pwm value
+# May need to add mid lower and upper configurable parameters
 
 import sys
 import time
@@ -14,63 +14,59 @@ from libs.ABE_ServoPi import PWM
 from libs.ABE_helpers import ABEHelpers
 import RPi.GPIO as GPIO
 
+
 class MotorDriver:
-	def __init__(self, pwmPort, dirPort, invert):
-		# Initialise the PWM device using the default address
-		i2c_helper = ABEHelpers()
-		bus = i2c_helper.get_smbus()
-		
-		self.invert = invert
-		self.pwm = PWM(bus, 0x40)
-		self.pwm.set_pwm_freq(50) # Set frequency to 60 Hz
-		self.pwm.output_enable()
-		self.pwmPort = pwmPort
-		self.dirPort = dirPort
-	
-		self.upper = 4095  #0 -> 4095
+    def __init__(self, pwmPort, dirPort, invert):
+        # Initialise the PWM device using the default address
+        i2c_helper = ABEHelpers()
+        bus = i2c_helper.get_smbus()
 
-		#init GPIO
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(dirPort, GPIO.OUT)
-		
+        self.invert = invert
+        self.pwm = PWM(bus, 0x40)
+        self.pwm.set_pwm_freq(50)  # Set frequency to 60 Hz
+        self.pwm.output_enable()
+        self.pwmPort = pwmPort
+        self.dirPort = dirPort
 
-	#set a value from -1 to 1
-	def setValue(self, value):
-		inVal = float(value)
-		outVal = 0
-		if (inVal > 1 or inVal < -1):
-			raise Exception("Value out of bounds, [-1, 1]")
-	
-		outVal = abs(inVal) * self.upper
+        self.upper = 4095  # 0 -> 4095
 
-		#Set Direction
-		GPIO.output(self.dirPort, ((inVal < 0) ^ self.invert))
+        # init GPIO
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(dirPort, GPIO.OUT)
 
+    # set a value from -1 to 1
+    def setValue(self, value):
+        inVal = float(value)
+        outVal = 0
+        if (inVal > 1 or inVal < -1):
+            raise Exception("Value out of bounds, [-1, 1]")
 
-		#print outVal #<-- for debugging uncomment	
-		self.pwm.set_pwm(self.pwmPort, 0, int(outVal))
+        outVal = abs(inVal) * self.upper
 
+        # Set Direction
+        GPIO.output(self.dirPort, ((inVal < 0) ^ self.invert))
 
-	#set the motor to off
-	def off(self):
-		self.pwm.set_pwm(self.pwmPort, 0, 0)
+        # print outVal #<-- for debugging uncomment
+        self.pwm.set_pwm(self.pwmPort, 0, int(outVal))
+
+    # set the motor to off
+    def off(self):
+        self.pwm.set_pwm(self.pwmPort, 0, 0)
 
 
-#===Test driver to control a servo===
+# ===Test driver to control a servo===
 def testDriver():
-	pwmPort = int(input("Enter a PWM port: "))
-	dirPort = int(input("Enter a dir port: "))
-	ctrl = MotorDriver(pwmPort, dirPort)
+    pwmPort = int(input("Enter a PWM port: "))
+    dirPort = int(input("Enter a dir port: "))
+    ctrl = MotorDriver(pwmPort, dirPort)
 
-	while (True):
-		value = input("Enter a value from -1 to 1: ")
-		if str(value) == "off":
-			ctrl.off()
-		else:	
-			ctrl.setValue(float(value))
+    while (True):
+        value = input("Enter a value from -1 to 1: ")
+        if str(value) == "off":
+            ctrl.off()
+        else:
+            ctrl.setValue(float(value))
 
 
 if __name__ == "__main__":
-	testDriver()
-
-
+    testDriver()
