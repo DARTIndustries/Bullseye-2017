11482f844945dd0f277381b08f385b7e58e60d60
Adam Feldscher <aff39@drexel.edu>
2018-04-22 11:33:36 -0400

    testing vectors

--- src/DART/Bullseye/Utils/VectorToMotorConverter.py
+++ src/DART/Bullseye/Utils/VectorToMotorConverter.py
@@ -1,16 +1,15 @@
 import sys
 from sympy import *
+
 sys.path.insert(0, '../../../')
 from DART.Bullseye.Models.Types import Coordinate
 
 
 class VectorToMotorConverter:
 
-
     @staticmethod
     def convert(movementVector: Coordinate, angleVector: Coordinate) -> tuple:
         pass
-
 
     @staticmethod
     def movementVals(movementVector: Coordinate) -> tuple:
@@ -20,9 +19,15 @@ class VectorToMotorConverter:
             [0.17, 1, 0.17, movementVector.z]
         ])
 
-        rref= matrix.rref()
+        # matrix = Matrix([
+        #     [.5, 0, -.5, movementVector.x],
+        #     [.5, 0, .5, movementVector.y],
+        #     [0.17, 1, 0.17, movementVector.z]
+        # ])
 
-        if not rref[1] == [0, 1, 2]:
+        rref = matrix.rref()
+
+        if not rref[1] == (0, 1, 2):
             print("Bad RREF!!!")
 
         reduced = rref[0]
@@ -31,7 +36,7 @@ class VectorToMotorConverter:
         m2 = reduced[7]
         m4 = reduced[11]
 
-
+        return m0, m2, m4
 
         # m0 = numpy.dot(movementVector.toList(), (1, 1, 0.17))
         # m1 = numpy.dot(movementVector.toList(), (-1, 1, 0.17))
@@ -40,3 +45,10 @@ class VectorToMotorConverter:
         # m4= numpy.dot(movementVector.toList(), (-1, 1, 0.17))
         # m5 = numpy.dot(movementVector.toList(), (1, 1, 0.17))
 
+
+def testDriver():
+    print(VectorToMotorConverter.movementVals(Coordinate(1, 0.2, 0)))
+
+
+if __name__ == "__main__":
+    testDriver()
